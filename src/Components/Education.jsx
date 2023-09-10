262ebd6d75effa3bef1f5e9f53d6188b4afa1b6a
Peshimam Mohammed Junaid <118809036+junaidpeshimam@users.noreply.github.com>
2023-09-10 12:24:39 +0000

    Added my education details to the Education component

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 
 const Education = () => {
+  const educationStyles = {
+    backgroundColor: "#F2E2C7", // Background color
+    padding: "20px", // Padding for the section
+    borderRadius: "10px", // Rounded corners
+    boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)", // Box shadow for a subtle effect
+  };
+
+  const schoolStyles = {
+    borderBottom: "2px solid #6C63FF", // Border color for each school
+    padding: "10px 0", // Padding for each school item
+    marginBottom: "10px", // Spacing between schools
+  };
+
   return (
-    <section className="light" id="education">
-      <h2>Education</h2>
-      <div>
-        <h3>Schooling:</h3>
-        <p>Sri Lakshmi High School</p>
+    <section style={educationStyles} id="education">
+      <h2 style={{ color: "#6C63FF" }}>My Education</h2>
+      <div style={schoolStyles}>
+        <h3 style={{ color: "#FF8C42" }}>Sri Lakshmi High School</h3>
+        <p>Completed my schooling with flying colors.</p>
       </div>
-      <div>
-        <h3>Higher School:</h3>
-        <p>Sri Chaitanya Junior College</p>
+      <div style={schoolStyles}>
+        <h3 style={{ color: "#FF8C42" }}>Sri Chaitanya Junior College</h3>
+        <p>Successfully completed my Higher School education.</p>
       </div>
-      <div>
-        <h3>Current Graduation:</h3>
-        <p>G Pullaiah College of Engineering and Technology</p>
+      <div style={schoolStyles}>
+        <h3 style={{ color: "#FF8C42" }}>G Pullaiah College of Engineering and Technology</h3>
+        <p>Currently pursuing my graduation in an exciting engineering journey.</p>
       </div>
     </section>
   );
