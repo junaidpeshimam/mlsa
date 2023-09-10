ab1ffa8df53e5333984ac98ad7ece4fe1b2c5466
Peshimam Mohammed Junaid <118809036+junaidpeshimam@users.noreply.github.com>
2023-09-10 12:16:35 +0000

    Added my education details to the Education component

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,6 +4,18 @@ const Education = () => {
   return (
     <section className="light" id="education">
       <h2>Education</h2>
+      <div>
+        <h3>Schooling:</h3>
+        <p>Sri Lakshmi High School</p>
+      </div>
+      <div>
+        <h3>Higher School:</h3>
+        <p>Sri Chaitanya Junior College</p>
+      </div>
+      <div>
+        <h3>Current Graduation:</h3>
+        <p>G Pullaiah College of Engineering and Technology</p>
+      </div>
     </section>
   );
 };
