43a56ec8e0cabae40b36ff10e92407023d2ff89d
Peshimam Mohammed Junaid <118809036+junaidpeshimam@users.noreply.github.com>
2023-09-10 12:40:36 +0000

    Added my education details

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -6,29 +6,29 @@ const Education = () => {
     {
       institution: "Sri Lakshmi High School",
       degree: "Schooling",
-      year: "2000-2012",
+      year: "2009-2019",
     },
     {
-      institution: "Sri Chaitany Junior College",
+      institution: "Sri Chaitanya Junior College",
       degree: "Higher School",
-      year: "2012-2014",
+      year: "2019-2021",
     },
     {
-      institution: "Coursera and Google",
+      institution: "Coursera",
       degree: "UX Design Professional Certificate",
       year: "2022",
     },
     {
-      institution: "G Pullaiah College of Engineering and Technology",
+      institution: "G Pullaiah College of Engineering and Technoloy",
       degree: "Computer Science and Engineering",
-      year: "2021-2025",
+      year: "2021 - 2025",
     },
   ];
 
   return (
     <section className="light" id="education">
-      <h2>Education</h2>
-      <div className="education-grid">
+      <h2 style={{ textAlign: "center" }}>Education</h2>
+      <div className="education-grid" style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         {educationDetails.map((edu, index) => (
           <div className="education-cell" key={index}>
             <h3>{edu.institution}</h3>
