9c9ed1c204ceeb7f5630fcae58dade7e7694f755
Peshimam Mohammed Junaid <118809036+junaidpeshimam@users.noreply.github.com>
2023-09-10 12:48:20 +0000

    Added more

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -9,19 +9,19 @@ const Education = () => {
       year: "2009-2019",
     },
     {
-      institution: "Sri Chaitanya Junior College",
+      institution: "Sri Chaitany Junior College",
       degree: "Higher School",
       year: "2019-2021",
     },
     {
-      institution: "Coursera",
-      degree: "UX Design Professional Certificate",
-      year: "2022",
+      institution: "G Pullaiah College of Engineering and Technology",
+      degree: "Bachelor's in Engineering",
+      year: "2021-2025",
     },
     {
-      institution: "G Pullaiah College of Engineering and Technoloy",
-      degree: "Computer Science and Engineering",
-      year: "2021 - 2025",
+      institution: "Coursera",
+      degree: "UX Design Professional Certificate",
+      year: "6 Months",
     },
   ];
 
@@ -30,7 +30,7 @@ const Education = () => {
       <h2 style={{ textAlign: "center" }}>Education</h2>
       <div className="education-grid" style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         {educationDetails.map((edu, index) => (
-          <div className="education-cell" key={index}>
+          <div className="box" key={index}>
             <h3>{edu.institution}</h3>
             <p>{edu.degree}</p>
             <p>{edu.year}</p>
